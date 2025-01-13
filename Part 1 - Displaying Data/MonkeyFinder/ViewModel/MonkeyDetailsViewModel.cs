3dddce6371e74c3ffafed3845cb7238a19c14ab9
Kristaps Dzenis (dzenisk) <dzenisk@coventry.ac.uk>
2025-01-13 15:13:02 +0000

    Final chapter

--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeyDetailsViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeyDetailsViewModel.cs
@@ -4,11 +4,37 @@
 [QueryProperty("Monkey", "Monkey")]
 public partial class MonkeyDetailsViewModel : BaseViewModel
 {
-    public MonkeyDetailsViewModel() 
+    // function to check availibility of map
+    IMap map;
+    public MonkeyDetailsViewModel(IMap map) 
     {
-        
+        this.map = map;
     }
 
     [ObservableProperty]        // links monkey to generated mvvm code
     Monkey monkey;
+
+    // RelayCommand is part pf mvvm generated code which converts asynchroniase function OpenMapAsync being callable through event handler
+    // Runs each time when get open map  button is clicked on details page 
+    [RelayCommand]
+    async Task OpenMapAsync()
+    {
+        try
+        {
+            // Open external map displaying location of current monkey
+            await map.OpenAsync(Monkey.Longitude, Monkey.Latitude,
+                new MapLaunchOptions
+                {
+                    Name = Monkey.Name,
+                    NavigationMode = NavigationMode.None,
+                });
+        }
+        catch (Exception ex)
+        {
+            // If error happens write error info to console and display error message in app
+            Debug.WriteLine(ex);
+            await Shell.Current.DisplayAlert("Error!", $"Unable to open map:{ex.Message}", "OK");
+        }
+    }
+
 }
--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeysViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -11,13 +11,63 @@ public partial class MonkeysViewModel : BaseViewModel
 
     public ObservableCollection<Monkey> Monkeys { get; } = new();   // data structure type where dynamically can be stored objects of monkey class
 
-    // function which defines app title and ?calls upon monkeyservice object?  runs on launch of app
-    public MonkeysViewModel(MonkeyService monkeyService)
+    // function to assign title and check availibility of monkey service, internet connectivity, and geolocator on launch of app
+    IConnectivity connectivity;
+    IGeolocation geolocation;
+    public MonkeysViewModel(MonkeyService monkeyService, IConnectivity connectivity, IGeolocation geolocation)
     {
         Title = "Monkey Finder";
         this.monkeyService = monkeyService;
-
+        this.connectivity = connectivity;
+        this.geolocation = geolocation;
     }
+
+    [ObservableProperty]            // links isRefreshing to generated mvvm code
+    bool isRefreshing;
+
+    // RelayCommand is part pf mvvm generated code which converts asynchroniase function GetClosestMonkeyAsync being callable through event handler
+    // Runs each time when get closest monkey  button is clicked on main page 
+    [RelayCommand]
+    async Task GetClosestMonkeyAsync()
+    {
+        // If IsBusy or count of monkeys is 0 do nothing
+        if(IsBusy || Monkeys.Count == 0) return;
+        
+        try
+        {
+            // retreive last known location of the device
+            var location = await geolocation.GetLastKnownLocationAsync();
+            // if there is no known last location do following
+            if(location == null)
+            {
+                // retreive current location of the device
+                location = await geolocation.GetLocationAsync(
+                    new GeolocationRequest
+                    {
+                        DesiredAccuracy = GeolocationAccuracy.Medium,
+                        Timeout = TimeSpan.FromSeconds(30),
+                    });
+            }
+
+            if (location == null) return;           // if there is no location do nothing
+
+            // find closest monkey based on location of the device
+            var first = Monkeys.OrderBy(m => location.CalculateDistance(location.Latitude, location.Longitude, DistanceUnits.Kilometers)).FirstOrDefault();
+
+            if(first == null) return;               // if there is no closest monkey do nothing
+
+            // Display error message in app with closest monkey
+            await Shell.Current.DisplayAlert("Closest Monkey", $"{first.Name} in {first.Location}", "OK");
+
+        }
+        catch (Exception ex)
+        {
+            // If error happens write error info to console and display error message in app
+            Debug.WriteLine(ex);
+            await Shell.Current.DisplayAlert("Error!", $"Unable to get closest monkey:{ex.Message}", "OK");
+        }
+    }
+
 
     // RelayCommand is part pf mvvm generated code which converts asynchroniase function GoToDetailsAsync being callable through event handler
     // Runs each time when monkey is clicked on main page and directs to create details page for clicked monkey
@@ -44,6 +94,13 @@ public partial class MonkeysViewModel : BaseViewModel
         // If IsBusy is true do nothing, otherwise try following instructions
         try
         {
+            // Display error message in app if there is no internet connection
+            if (connectivity.NetworkAccess != NetworkAccess.Internet)
+            {
+                await Shell.Current.DisplayAlert("Internet Issue", $"Check your internet connection", "OK");
+                return;
+            }
+            
             IsBusy = true;                                  // IsBusy is true
             var monkeys = await monkeyService.GetMonkeys(); // Calls upon monkeyservice object to retreive monkey data and store it in monkeys variable
 
@@ -67,6 +124,7 @@ public partial class MonkeysViewModel : BaseViewModel
         finally
         {
             IsBusy = false;                         // IsBusy is false
+            IsRefreshing = false;                   // IsRefreshing is false
         }
     }
 }
